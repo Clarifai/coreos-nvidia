773c764789e3a678ae6f8769940bdd886d3eeed9
Rudi Chiarito <rudi@clarifai.com>
2017-01-24 16:02:50 -0500

    Container build: exit early, increase parallelism
    Hardcode parallelism to 4 for now.
    Add --emerge-sources for emerge not to use binary packages (not yet supported by the caller, build.sh)
    Make sure we print the nvidia-installer logs no matter when or how we exit.

--- _container_build.sh
+++ _container_build.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
+# Default: use binary packages instead of building everything from source
+EMERGE_SOURCE_FLAGS=gK
+while :; do
+  case $1 in
+    --emerge-sources)
+      EMERGE_SOURCE_FLAGS=
+      ;;
+    *)
+      break
+  esac
+  shift
+done
+
+
 VERSION=$1
 echo Building ${VERSION}
 
-emerge-gitclone
+function finish {
+  cat /nvidia_installers/NVIDIA-Linux-x86_64-${VERSION}/nvidia-installer.log
+}
 
-emerge -gKq coreos-sources
+set -e
+trap finish exit
+
+emerge-gitclone
+emerge -${EMERGE_SOURCE_FLAGS}q --jobs 4 --load-average 4 coreos-sources
+
 cd /usr/src/linux
 cp /lib/modules/*-coreos*/build/.config .config
 
@@ -16,5 +37,3 @@ cd /nvidia_installers/NVIDIA-Linux-x86_64-${VERSION}
 ./nvidia-installer -s -n --kernel-source-path=/usr/src/linux \
   --no-check-for-alternate-installs --no-opengl-files \
   --kernel-install-path=${PWD} --log-file-name=${PWD}/nvidia-installer.log
-
-cat nvidia-installer.log
