18edfe3a6fb7071cde31c01c7a2e365fa5a43f42
Rudi Chiarito <rudi@clarifai.com>
2017-01-24 16:08:53 -0500

    Improve error handling, add --emerge-sources
    Use errexit (set -e) to bail out early when errors occur.
    Don't use --share-system. It's going away in systemd 232 anyway.
    Reworked command line parsing.
    More verbose output.

--- build.sh
+++ build.sh
@@ -3,11 +3,28 @@
 # Build NVIDIA drivers for a given CoreOS version
 #
 
-if [ x"$1" == x"--keep" ]
-then
-  KEEP_CONTAINER=1
+KEEP_CONTAINER=false
+EMERGE_SOURCES=""
+while :; do
+  case $1 in
+    --keep)
+      KEEP_CONTAINER=true
+      ;;
+    --emerge-sources)
+      EMERGE_SOURCES=$1
+      ;;
+    -?*)
+      echo Unknown flag $1
+      exit 1
+      ;;
+    *)
+      break
+  esac
   shift
-fi
+done
+
+echo "Keeping container around after build: ${KEEP_CONTAINER}"
+echo "Additional flags: ${EMERGE_SOURCES}"
 
 DRIVER_VERSION=${1:-367.57}
 COREOS_TRACK=${2:-beta}
@@ -19,15 +36,28 @@ DEV_CONTAINER=coreos_developer_container.bin.${COREOS_VERSION}
 WORK_DIR=pkg/run_files/${COREOS_VERSION}
 ORIGINAL_DIR=${PWD}
 
-function finish {
-  if [ "${KEEP_CONTAINER}" != "1" ]
-  then
-    cd ${ORIGINAL_DIR}
-    rm -Rf ${DEV_CONTAINER} ${WORK_DIR}/${DRIVER_ARCHIVE} tmp
-  fi
+function onerr {
+  echo Caught error
+  finish
 }
 
-trap finish exit
+function onexit {
+  finish
+}
+
+function finish {
+  if [ "${KEEP_CONTAINER}" != "true" ]
+  then
+    cd ${ORIGINAL_DIR}
+    echo Cleaning up
+    sudo rm -Rf ${DEV_CONTAINER} ${WORK_DIR}/${DRIVER_ARCHIVE} tmp
+  fi
+  exit
+}
+
+set -e
+trap onerr ERR
+trap onexit exit
 
 if [ ! -f ${DEV_CONTAINER} ]
 then
@@ -56,14 +86,19 @@ cp -ul ${DRIVER_ARCHIVE_PATH} ${WORK_DIR}
 
 cd ${WORK_DIR}
 chmod +x ${DRIVER_ARCHIVE}.run
-rm -Rf ./${DRIVER_ARCHIVE}
+sudo rm -Rf ./${DRIVER_ARCHIVE}
 ./${DRIVER_ARCHIVE}.run -x -s
 cd ${ORIGINAL_DIR}
 
-sudo systemd-nspawn -i ${DEV_CONTAINER} --share-system \
+# Ignore errors just for nspawn
+set +e
+trap ":" ERR
+sudo systemd-nspawn -i ${DEV_CONTAINER} \
   --bind=${PWD}/_container_build.sh:/build.sh \
   --bind=${PWD}/${WORK_DIR}:/nvidia_installers \
-  /bin/bash -x /build.sh ${DRIVER_VERSION}
+  /bin/bash -x /build.sh ${EMERGE_SOURCES} ${DRIVER_VERSION}
+trap onerr ERR
+set -e
 
 sudo chown -R ${UID}:${GROUPS[0]} ${PWD}/${WORK_DIR}
 
